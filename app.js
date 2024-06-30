5d06e421f3c3d3743ee887889dc23d55f26b18cf
Dawn <120154112+zen-da-baker@users.noreply.github.com>
2024-06-29 22:02:01 -0700

    Quiz usable

--- app.js
+++ app.js
@@ -10,41 +10,60 @@ function inputTest() {
 
 document.getElementById('input').addEventListener('change', inputTest);
 
- 
+let question = 1;
+questionNum.innerHTML = question;
 
-questionNum.innerHTML = 1;
+let correct = 0;
+let incorrect = 0;
+
+let correctRes = '<p>Correct!</p> <button onclick="main()">Would you like to play again?</button>';
+let incorrectRes = '<p>Incorrect</p> <button onclick="main()">Would you like to play again?</button>';
 
 function randomNum() {
     return Math.floor(Math.random() * 100);
 }
 
-let num1Value = randomNum();
-let num2Value = randomNum();
-
-console.log("Num 1: " + num1Value);
-console.log("Num 2: " + num2Value);
-
 function main() {
+
+    let num1Value = randomNum();
+    let num2Value = randomNum();
+
+    console.log("Num 1: " + num1Value);
+    console.log("Num 2: " + num2Value);
+
+
+    function capture() {
+
+        if (input.value == NaN) {
+            input.value = parseInt(input.value);
+        }
+
+        if (input.value == num1Value + num2Value) {
+            response.innerHTML = correctRes;
+            correct++;
+            console.log('Correct!');
+        } else {
+            response.innerHTML = incorrectRes;
+            incorrect++;
+            console.log('Incorrect!');
+        }
+   
+    }
+
+    document.getElementById('submit').addEventListener('click', capture);
+
     num1.innerHTML = num1Value;
     num2.innerHTML = num2Value;
 
+    questionNum.innerHTML = question++;
+
+    console.log(correct);
+    console.log(incorrect);
     
 }
 
-function capture() {
 
-    if (input.value == NaN) {
-        input.value = parseInt(input.value);
-    }
-    
-    if (input.value == num1Value + num2Value) {
-        response.innerHTML = 'Correct!';
-        console.log('Correct!');
-    } else {
-        response.innerHTML = 'Incorrect';
-        console.log('Incorrect!');
-    }
-    questionNum.innerHTML++;
-}
+
+
 
 
