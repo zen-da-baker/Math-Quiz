e17d12d5389a678d32d98bb0affc35dfdd863f5d
Dawn <120154112+zen-da-baker@users.noreply.github.com>
2024-06-29 21:10:29 -0700

    Inputs valid

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 let num1 = document.getElementById('num1');
 let num2 = document.getElementById('num2');
-let input = document.getElementById('input').value;
+let input = document.getElementById('input');
 let response = document.getElementById('response');
+let questionNum = document.getElementById('questionNum');
+
+function inputTest() {
+    console.log(input.value);
+} 
+
+document.getElementById('input').addEventListener('change', inputTest);
+
+ 
+
+questionNum.innerHTML = 1;
 
 function randomNum() {
     return Math.floor(Math.random() * 100);
@@ -10,15 +21,30 @@ function randomNum() {
 let num1Value = randomNum();
 let num2Value = randomNum();
 
+console.log("Num 1: " + num1Value);
+console.log("Num 2: " + num2Value);
+
 function main() {
     num1.innerHTML = num1Value;
     num2.innerHTML = num2Value;
+
+    
 }
 
 function capture() {
-    if (parseInt(input) == num1Value + num2Value) {
+
+    if (input.value == NaN) {
+        input.value = parseInt(input.value);
+    }
+    
+    if (input.value == num1Value + num2Value) {
         response.innerHTML = 'Correct!';
+        console.log('Correct!');
     } else {
         response.innerHTML = 'Incorrect';
+        console.log('Incorrect!');
     }
+    questionNum.innerHTML++;
 }
+
+
