7b50a87073d7266a7e449f36a8454fc5a6341e10
Dawn <120154112+zen-da-baker@users.noreply.github.com>
2024-06-29 23:28:08 -0700

    functions separated and scope correct

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ questionNum.innerHTML = question;
 let correct = 0;
 let incorrect = 0;
 
-let correctRes = '<p>Correct!</p> <button onclick="main()">Would you like to play again?</button>';
-let incorrectRes = '<p>Incorrect</p> <button onclick="main()">Would you like to play again?</button>';
+
+let num1Value = 0;
+let num2Value = 0;
 
 function easyRand() {
     return Math.floor(Math.random() * 10);
@@ -36,53 +37,74 @@ function veryHardRand () {
     return Math.floor(Math.random() * 10000);
 }
 
-function main() {
-
+function reset() {
     response.innerHTML = '';
     input.value = '';
+}
 
-    let num1Value = 0;
-    let num2Value = 0;
-
-    if (question <= 5) {
+function scaleDifficulty() {
+    if (correct <= 5) {
         num1Value = easyRand();
         num2Value = easyRand();
-    } else if (question > 5 && question < 10) {
+    } else if (correct > 5 && correct < 10) {
         num1Value = mediumRand();
         num2Value = mediumRand();
-    } else if (question > 10 && question < 15) {
+    } else if (correct > 10 && correct < 15) {
         num1Value = hardRand();
         num2Value = hardRand();
+    } else if (correct > 15) {
+        num1Value = veryHardRand();
+        num2Value = veryHardRand();
     } else {
         num1Value = veryHardRand();
         num2Value = veryHardRand();
     }
+}
+
+function correctRes(correct, incorrect) {
+    return `
+        <p>Correct!</p>
+        <button onclick="main(); reset()">Would you like to play again?</button>
+        <p>Correct: ${correct}</p>
+        <p>Incorrect: ${incorrect}</p>
+    `;
+}
+
+function incorrectRes (correct, incorrect) { 
+    return `
+        <p>Incorrect</p>
+        <button onclick="main(); reset()">Would you like to play again?</button>
+        <p>Correct: ${correct}</p>
+        <p>Incorrect: ${incorrect}</p>
+    `;
+}
+
+function capture() {
 
     
 
-    console.log("Num 1: " + num1Value);
-    console.log("Num 2: " + num2Value);
 
-
-    function capture() {
-
-        if (input.value == NaN) {
-            input.value = parseInt(input.value);
-        }
-
-        if (input.value == num1Value + num2Value) {
-            response.innerHTML = correctRes;
-            correct++;
-            console.log('Correct!');
-        } else {
-            response.innerHTML = incorrectRes;
-            incorrect++;
-            console.log('Incorrect!');
-        }
-   
+    if (input.value == NaN) {
+        input.value = parseInt(input.value);
     }
 
-    submit.addEventListener('click', capture);
+    if (input.value == num1Value + num2Value) {
+        correct++;
+        response.innerHTML = correctRes(correct, incorrect);
+        console.log('Correct!');
+    } else {
+        incorrect++;
+        response.innerHTML = incorrectRes(correct, incorrect);
+        console.log('Incorrect!');
+    }
+
+}
+
+function main() {
+    scaleDifficulty();
+
+    console.log("Num 1: " + num1Value);
+    console.log("Num 2: " + num2Value);
 
     num1.innerHTML = num1Value;
     num2.innerHTML = num2Value;
