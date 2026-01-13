bde5a4a9bb6d9a00864e53f8f72cebe0446e675d
Don <120154112+zen-da-baker@users.noreply.github.com>
2026-01-12 22:03:27 -0800

    All base javascript ignored

---
+++ express-server/src/client/serviceWorker.ts
@@ -0,0 +1,92 @@
+// List of all file urls for this web application
+let knownFilePaths: Array<string> = [
+    "/",
+    "/index.html",
+    "/second.html",
+    "/app.js",
+    "/serviceWorker.js",
+    "/styles/style.css",
+    "/manifest.json"
+]
+
+// Cache name string
+let cacheName: string = "math-quiz-game-cache-v" + 1;
+
+// Cache all of the website paths using a for loop
+async function cacheAllFiles( event: any ) {
+
+    // Open the cache object
+    let cache = await self.caches.open( cacheName );
+
+    // Adds all known files to the cache based on their URLs
+    knownFilePaths.forEach( ( path: string ) => {
+
+        cache.add( path );
+
+    })
+
+}
+
+function networkRequest( event: any ) {
+
+    let { request } = event;
+
+    let cacheStorage = self.caches;
+
+    // Handle what is sent in response to the network request from the browser
+    event.respondWith(
+
+        // Perform the browser default fetch request first and detect if there is a connection error
+        fetch( request ).catch( ( err: any ) => {
+
+            console.log( err );
+
+            // If the method of the request is GET after an error is detected, respond with the cache
+            if ( request.method === "GET" ) {
+
+                // Open the cache name and find a match with the request
+                return cacheStorage.open( cacheName ).then( ( cache: Cache ) => {
+
+                    // Return the cached response
+                    return cache.match( request ).catch( ( err: any ) => {
+
+                        console.log( err );
+
+                    })
+
+                })
+        
+            }
+
+        })
+
+    )
+
+}
+
+async function addActiveListeners( event: any ) {
+
+    // If the cache has keys, cycle through them
+    const cacheKeys = await caches.keys();
+
+    // For each cache key in the list, check if it is current
+    cacheKeys.forEach( ( cacheKey: string ) => {
+
+        // If the cache key is not current, remove it from the cache
+        if ( cacheKey !== cacheName ) {
+
+            caches.delete( cacheKey );
+
+        }
+
+    })
+
+    event.srcElement.clients.claim();
+
+}
+
+self.addEventListener( "install", cacheAllFiles );
+
+self.addEventListener( "activate", addActiveListeners );
+
+self.addEventListener( "fetch", networkRequest);
