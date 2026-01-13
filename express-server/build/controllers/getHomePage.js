19c00d2e41b699d2c4909d4761710070a7fd48e4
Don <120154112+zen-da-baker@users.noreply.github.com>
2026-01-12 18:51:50 -0800

    Ignoring build

--- express-server/build/controllers/getHomePage.js
+++
@@ -1,4 +0,0 @@
-export async function getHomePage(req, res) {
-    console.log(req);
-    res.send("<h1>Hello World!</h1>");
-}
--- express-server/src/controllers/getHomePage.ts
+++ express-server/src/controllers/getHomePage.ts
@@ -1,7 +1,5 @@
 export async function getHomePage( req: any, res: any ) {
 
-    console.log( req );
-
     res.send("<h1>Hello World!</h1>");
 
 }
