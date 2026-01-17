fcd23e9aefa31dfb9fc3f32fc5e90af8528ef5e2
Don <120154112+zen-da-baker@users.noreply.github.com>
2026-01-16 18:42:37 -0800

    Leaderboard page added and accessible from memory

--- express-server/src/controllers/dashboard-router/dashboardRouter.ts
+++ express-server/src/controllers/dashboard-router/dashboardRouter.ts
@@ -8,6 +8,7 @@ import { getMathGamePage } from "../get-pages/dashboard/getMathGamePage.js";
 import { getSettingsPage } from "../get-pages/dashboard/getSettingsPage.js";
 import { getStorePage } from "../get-pages/dashboard/getStorePage.js";
 import { getStoryPage } from "../get-pages/dashboard/getStoryPage.js";
+import { getLeaderboardPage } from "../get-pages/dashboard/getLeaderboardPage.js";
 
 const dashboardRouter = express.Router();
 
@@ -22,5 +23,7 @@ dashboardRouter.get( "/settings", getSettingsPage );
 dashboardRouter.get( "/store", getStorePage );
 
 dashboardRouter.get( "/story", getStoryPage );
+
+dashboardRouter.get( "/leaderboard", getLeaderboardPage );
 
 export { dashboardRouter };
--- express-server/src/helpers/page-contents/pageContents.ts
+++ express-server/src/helpers/page-contents/pageContents.ts
@@ -4,8 +4,12 @@ let adminPagePath = "../../../../views/pages/dashboard/admin.html";
 
 let dashboardHomePagePath = "../../../../views/pages/dashboard/dashboardHome.html";
 
+let leaderboardPagePath = "../../../../views/pages/dashboard/leaderboard.html";
+
 let adminPageContent = await getPageContentHelper( adminPagePath );
 
 let dashboardHomePageContent = await getPageContentHelper( dashboardHomePagePath );
 
-export { adminPageContent, dashboardHomePageContent };
+let leaderboardPageContent = await getPageContentHelper( leaderboardPagePath );
+
+export { adminPageContent, dashboardHomePageContent, leaderboardPageContent };
